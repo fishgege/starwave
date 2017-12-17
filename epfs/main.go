a40af82ac12570d5f5155b3426d5dc650d37e3d4
Sam Kumar <samkumar99@gmail.com>
2017-12-16 21:21:13 -0800

    More progress on encryption-free IPFS utilities

--- epfs/main.go
+++ epfs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -15,6 +16,37 @@ func handle(err error) {
 	}
 }
 
+func addtofs(s *ipfs.Shell, nodeid string, path string, filehash string) string {
+	newroothash, err := s.Patch("/ipns/"+nodeid, "add-link", path, "/ipfs/"+filehash)
+	handle(err)
+
+	fmt.Printf("Updated directory tree: %s\n", newroothash)
+	return newroothash
+}
+
+func setnewroothash(s *ipfs.Shell, newroothash string) {
+	err := s.Publish("", newroothash)
+	handle(err)
+
+	fmt.Println("Published new root")
+}
+
+func validatewrite(s *ipfs.Shell, fullpath string) (string, string) {
+	idout, err := s.ID()
+	handle(err)
+
+	myself := idout.ID
+
+	if !strings.HasPrefix(fullpath, "/ipns/"+myself) {
+		fmt.Fprintf(os.Stderr, "Can only write to your own filesystem (can only write to /ipns/%s/*, tried to write to %s)\n", myself, fullpath)
+		os.Exit(2)
+	}
+
+	components := strings.SplitN(fullpath, "/", 4)
+
+	return myself, components[3]
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <command> [arguments...]\n", os.Args[0])
@@ -23,45 +55,55 @@ func main() {
 	cmd := os.Args[1]
 	args := os.Args[2:]
 	switch cmd {
-	case "add":
+	case "mkfile":
 		if len(args) != 1 && len(args) != 2 {
 			fmt.Fprintf(os.Stderr, "Usage: %s %s <dst> [<src>]\n", os.Args[0], cmd)
 			fmt.Fprintln(os.Stderr, "Reads from stdin if no <src> is provided")
 			os.Exit(2)
 		}
 		s := ipfs.NewLocalShell()
+		myself, path := validatewrite(s, args[0])
 
-		idout, err := s.ID()
-		handle(err)
-
-		myself := idout.ID
-
-		components := strings.Split(args[0], "/")
-
-		if components[2] != myself {
-			fmt.Fprintf(os.Stderr, "Can only write to your own filesystem (you are %s, tried to write to filesystem of %s)\n", myself, components[2])
-			os.Exit(2)
+		var newfile io.Reader
+		if len(args) == 2 {
+			newfile = strings.NewReader(args[1])
+		} else {
+			newfile = os.Stdin
 		}
 
-		hash, err := s.Add(strings.NewReader(args[1]))
+		hash, err := s.Add(newfile)
 		handle(err)
-
 		fmt.Printf("Added file: %s\n", hash)
 
-		newroothash, err := s.Patch(strings.Join(components[:3], "/"), "add-link", strings.Join(components[3:], "/"), "/ipfs/"+hash)
+		newroothash := addtofs(s, myself, path, hash)
+		setnewroothash(s, newroothash)
+	case "mkdir":
+		if len(args) != 1 {
+			fmt.Fprintf(os.Stderr, "Usage: %s %s <target>\n", os.Args[0], cmd)
+			os.Exit(3)
+		}
+		s := ipfs.NewLocalShell()
+		myself, path := validatewrite(s, args[0])
+
+		hash, err := s.NewObject("unixfs-dir")
 		handle(err)
+		fmt.Printf("Added directory: %s\n", hash)
 
-		fmt.Printf("Updated directory tree: %s\n", newroothash)
-
-		err = s.Publish("", newroothash)
-		handle(err)
-
-		fmt.Println("Published new root")
+		newroothash := addtofs(s, myself, path, hash)
+		setnewroothash(s, newroothash)
 	case "unlink":
 		if len(args) != 1 {
 			fmt.Fprintf(os.Stderr, "Usage: %s %s <target>\n", os.Args[0], cmd)
 			os.Exit(3)
 		}
+		s := ipfs.NewLocalShell()
+		myself, path := validatewrite(s, args[0])
+
+		newroothash, err := s.Patch("/ipns/"+myself, "rm-link", path)
+		handle(err)
+		fmt.Printf("Updated directory tree: %s\n", newroothash)
+
+		setnewroothash(s, newroothash)
 	case "ls":
 		if len(args) != 1 {
 			fmt.Fprintf(os.Stderr, "Usage: %s %s <target>\n", os.Args[0], cmd)
@@ -82,7 +124,7 @@ func main() {
 	case "--help":
 		fallthrough
 	case "help":
-		fmt.Fprintln(os.Stderr, "Valid commands are: add unlink ls cat")
+		fmt.Fprintln(os.Stderr, "Valid commands are: mkfile mkdir unlink ls cat")
 		os.Exit(6)
 	}
 }
